034fb13a6775698586f07d9ea815a6a685d3a01f
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-12-28 16:41:19 +0700

    commit ngay 28/12

--- internet-store-management/src/main/java/c0520g1/sprint_3/repository/ServicesRepository.java
+++ internet-store-management/src/main/java/c0520g1/sprint_3/repository/ServicesRepository.java
@@ -4,6 +4,8 @@ import c0520g1.sprint_3.model.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServicesRepository extends JpaRepository<Services, Long> {
 }
