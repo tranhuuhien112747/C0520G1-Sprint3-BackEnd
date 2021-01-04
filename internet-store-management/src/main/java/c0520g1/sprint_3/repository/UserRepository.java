eebd9f1b7cadb1cfde6d1bff59e92c385a336bea
Nguyentienhai1308 <tienhai19631959@gmail.com>
2021-01-04 16:14:53 +0700

    TIEN HAI COMMIT

--- internet-store-management/src/main/java/c0520g1/sprint_3/repository/UserRepository.java
+++ internet-store-management/src/main/java/c0520g1/sprint_3/repository/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByUsername(String username);
+    User findUserByEmail(String email);
 
     Boolean existsByUsername(String username);
 
